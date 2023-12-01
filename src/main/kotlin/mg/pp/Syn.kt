7e6d9c9476a5bb48eb05e50211a331d43b811de3
Mika Granqvist <mika.granqvist@outlook.com>
2023-12-01 14:49:50 +0200

    Initial commit of Syn.

---
+++ src/main/kotlin/mg/pp/Syn.kt
@@ -0,0 +1,6 @@
+package mg.pp
+
+class Syn {
+
+
+}
---
+++ src/test/kotlin/mg/pp/SynTest.kt
@@ -0,0 +1,16 @@
+package mg.pp
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class SynTest {
+
+
+    @Test
+    fun test1() {
+
+
+
+        assertTrue(false)
+    }
+}
