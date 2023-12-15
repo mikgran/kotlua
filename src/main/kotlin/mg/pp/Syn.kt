57b7664e1816b5fe08dbbd4d366a550708c39fd2
Mika Granqvist <mika.granqvist@outlook.com>
2023-12-15 17:18:56 +0200

    Added reflection to access the Parser class.

--- src/main/kotlin/mg/pp/Syn.kt
+++ src/main/kotlin/mg/pp/Syn.kt
@@ -1,6 +1,49 @@
 package mg.pp
 
+import kastree.ast.Node
+import kastree.ast.psi.Parser
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.MessageRenderer
+import org.jetbrains.kotlin.cli.common.messages.PrintingMessageCollector
+import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
+import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
+import org.jetbrains.kotlin.com.intellij.openapi.util.Disposer
+import org.jetbrains.kotlin.config.CompilerConfiguration
+
 class Syn {
 
+    companion object {
+
+        fun parse(): Node.File {
+
+            val parser = Parser()
+
+            val compilerConfiguration = CompilerConfiguration()
+            compilerConfiguration.put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, PrintingMessageCollector(System.err, MessageRenderer.PLAIN_FULL_PATHS, false))
+            val project = KotlinCoreEnvironment.createForProduction(
+                    Disposer.newDisposable(),
+                    compilerConfiguration,
+                    EnvironmentConfigFiles.JVM_CONFIG_FILES
+            ).project
+
+            val field = parser::class.java.getDeclaredField("proj\$delegate")
+            field.isAccessible = true
+            field.set(parser, lazyOf(project))
+
+            return parser.parseFile(codeString)
+        }
+
+        val codeString =
+"""
+    package foo
+
+    fun bar() {
+        // Print hello
+        println("Hello, World!")
+    }
+    fun baz() = println("Hello, again!")
+""".trimIndent()
+    }
 
 }
+
--- src/test/kotlin/mg/pp/SynTest.kt
+++ src/test/kotlin/mg/pp/SynTest.kt
@@ -1,16 +1,19 @@
 package mg.pp
 
-import org.junit.jupiter.api.Assertions.*
+import kastree.ast.Node
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class SynTest {
 
-    val testL1 = "{ k, v -> v }"
-
     @Test
     fun test1() {
 
+        val file: Node.File = Syn.parse()
 
+        file.decls.forEach {
+            println(it)
+        }
 
         assertTrue(true)
     }
